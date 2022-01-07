dc9ffd7e4d19693d64dca4b21229030ed71c790e
Koushikur Islam Shohag <ki.shohag@outlook.com>
2022-01-07 20:39:51 +0600

    Added AutoMapper with dependency injection

---
+++ TaskManagementSystem_WebApi/Business_Logic_Layer/Helpers/AutoMapperSettings.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Business_Entity_Layer.DTO;
+using Data_Access_Layer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Logic_Layer.Helpers
+{
+    public class AutoMapperSettings:Profile
+    {
+        public AutoMapperSettings()
+        {
+            CreateMap<Persons, PersonDto>().ReverseMap();
+            CreateMap<Tasks, TaskDto>().ReverseMap();
+            CreateMap<RequestLogs, RequestLogDto>().ReverseMap();
+            CreateMap<ExceptionLogs, ExceptionLogDto>().ReverseMap();
+        }
+    }
+}
--- TaskManagementSystem_WebApi/TaskManagementSystem_WebApi/Program.cs
+++ TaskManagementSystem_WebApi/TaskManagementSystem_WebApi/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 //builder.Services.Configure<ConnectionStrings>(builder.Configuration.GetSection("ConnectionStrings"));
 
